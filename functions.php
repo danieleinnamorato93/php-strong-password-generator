c1d36f98e25f279fd9429d633e3ba6cce3f36b4c
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-07 16:39:39 +0200

    Milestone #2

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+function getRandomPassword($length){
+    $charachters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    $password="";
+
+    for ($i=0; $i < $length ; $i++) { 
+     $index = rand(0, strlen($charachters) -1);
+     $password .=$charachters[$index];
+    }
+    return $password ;
+}
+if(isset($_GET['pwLength'])) {
+    $length = $_GET['pwLength'];
+    $newPassword = getRandomPassword($length);
+}
+
+?>
--- index.php
+++ index.php
@@ -1,22 +1,6 @@
 <?php
-function getRandomPassword($length){
-    $charachters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-    $password="";
-
-    for ($i=0; $i < $length ; $i++) { 
-     $index = rand(0, strlen($charachters) -1);
-     $password .=$charachters[$index];
-    }
-    return $password ;
-}
-if(isset($_GET['pwLength'])) {
-    $length = $_GET['pwLength'];
-    $newPassword = getRandomPassword($length);
-}
-
+include 'functions.php'
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
